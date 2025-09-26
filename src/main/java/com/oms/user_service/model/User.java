92db8032dbc3edade09f74498e0a596078750ddf
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2025-09-26 15:45:02 +0530

    adding required dependencies

---
+++ src/main/java/com/oms/user_service/model/User.java
@@ -0,0 +1,31 @@
+package com.oms.user_service.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class User {
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "_user_id_sequence_generator"
+    )
+    @SequenceGenerator(
+            name = "_user_id_sequence_generator",
+            sequenceName = "_user_id_sequence",
+            initialValue = 1,
+            allocationSize = 1
+    )
+    @Id
+    private long UserId;
+    private String UserName;
+    private Date DOB;
+    private boolean isAdmin;
+    private Date createdAt;
+    private Date updatedAt;
+}
