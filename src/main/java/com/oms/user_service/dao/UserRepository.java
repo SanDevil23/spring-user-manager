93b2042fe46d058a4e6d3f83bb7b1ba9e3f18d24
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2026-01-15 00:09:37 +0530

    adding custom id filter

--- src/main/java/com/oms/user_service/dao/UserRepository.java
+++ src/main/java/com/oms/user_service/dao/UserRepository.java
@@ -1,9 +1,22 @@
 package com.oms.user_service.dao;
 
 import com.oms.user_service.model.User;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    default User filterUserById(@NotNull Long userId){
+        List<User> userList = findAll();
+        for (User user : userList){
+            if (userId == user.getUserId()){
+                return user;
+            }
+        }
+        return null;
+    }
 }
